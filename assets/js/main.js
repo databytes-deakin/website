629c06364f7cd0de91b19009df47323c629bb5dc
jbertucci-deakin <jbertucci@deakin.edu.au>
2022-05-09 17:08:11 +1000

    Content Update

--- assets/js/main.js
+++ assets/js/main.js
@@ -149,23 +149,23 @@
    * Porfolio isotope and filter
    */
   window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
-    if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
+    let projectsContainer = select('.projects-container');
+    if (projectsContainer) {
+      let projectsIsotope = new Isotope(projectsContainer, {
+        itemSelector: '.projects-item',
         layoutMode: 'fitRows'
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let projectsFilters = select('#projects-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#projects-flters li', function(e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        projectsFilters.forEach(function(el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
 
-        portfolioIsotope.arrange({
+        projectsIsotope.arrange({
           filter: this.getAttribute('data-filter')
         });
 
@@ -175,16 +175,16 @@
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate projects lightbox 
    */
-  const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
+  const projectsLightbox = GLightbox({
+    selector: '.projects-lightbox'
   });
 
   /**
-   * Portfolio details slider
+   * projects details slider
    */
-  new Swiper('.portfolio-details-slider', {
+  new Swiper('.projects-details-slider', {
     speed: 400,
     loop: true,
     autoplay: {
